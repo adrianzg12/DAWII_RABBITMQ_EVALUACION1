a3ac5b2041e8e90a351f3940b0caefcef3f37715
AdrianLlontop <rizzaga1234@gmail.com>
2024-09-27 18:57:25 -0500

    estructura del proyecto

---
+++ src/main/java/pe/edu/cibertec/RabbitMQEvaluacionDAWII/RabbitMqEvaluacionDawiiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.RabbitMQEvaluacionDAWII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqEvaluacionDawiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqEvaluacionDawiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/RabbitMQEvaluacionDAWII/RabbitMqEvaluacionDawiiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.RabbitMQEvaluacionDAWII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqEvaluacionDawiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
