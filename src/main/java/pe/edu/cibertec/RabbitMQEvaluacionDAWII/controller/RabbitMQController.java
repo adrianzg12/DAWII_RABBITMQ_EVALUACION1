24a6c513be40e520fd8db4479c4c0754b77bfcd9
AdrianLlontop <rizzaga1234@gmail.com>
2024-09-27 19:39:44 -0500

    controller

--- src/main/java/pe/edu/cibertec/RabbitMQEvaluacionDAWII/controller/RabbitMQController.java
+++ src/main/java/pe/edu/cibertec/RabbitMQEvaluacionDAWII/controller/RabbitMQController.java
@@ -1,4 +1,20 @@
 package pe.edu.cibertec.RabbitMQEvaluacionDAWII.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.RabbitMQEvaluacionDAWII.enespera.producer.RabbitMQProducer;
+
+@RequiredArgsConstructor
+@RestController
 public class RabbitMQController {
+
+    private final RabbitMQProducer rabbitMQProducer;
+
+    @GetMapping("/enviarmsg")
+    public String enviarMensaje(@RequestParam String mensaje) {
+        rabbitMQProducer.enviarMensaje(mensaje);
+        return "Mensaje sobre el inventario: Stock " + mensaje;
+    }
 }
--- src/main/java/pe/edu/cibertec/RabbitMQEvaluacionDAWII/enespera/consumer/RabbitMQConsumer.java
+++ src/main/java/pe/edu/cibertec/RabbitMQEvaluacionDAWII/enespera/consumer/RabbitMQConsumer.java
@@ -10,6 +10,6 @@ public class RabbitMQConsumer {
 
     @RabbitListener(queues = "queue")
     public void consumirMensaje(String mensaje){
-        log.info("Mensaje recibido" + mensaje + "inicio de operación");
+        log.info("Mensaje recibido"+mensaje+"Enviando a la web");
     }
 }
