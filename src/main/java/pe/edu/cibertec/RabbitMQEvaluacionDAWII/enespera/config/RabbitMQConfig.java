b1a0fb0760c4ff74c236d923e4d51fff258ddf19
AdrianLlontop <rizzaga1234@gmail.com>
2024-09-27 19:17:15 -0500

    config

---
+++ src/main/java/pe/edu/cibertec/RabbitMQEvaluacionDAWII/enespera/config/RabbitMQConfig.java
@@ -0,0 +1,27 @@
+package pe.edu.cibertec.RabbitMQEvaluacionDAWII.enespera.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue queue(){
+        return new Queue("queue", true);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange("exchange");
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with("routing");
+    }
+}
